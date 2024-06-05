3a1e266ce1647534d2f71d4c7eaf884c020932c5
Coppermine-SP <son5121502@gmail.com>
2024-06-05 13:59:48 +0000

    init: 새 C 프로젝트 생성

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/*
+    main.c - bstCalc (2024-1 Data Structure Assignment)
+    Copyright (C) 2024 Coppermine-SP - <국립창원대학교 컴퓨터공학과 20233063 손유찬>
+*/ 
+
+#include <stdio.h>
+#include <stdbool.h>
+#include "bst.h"
+#include "calc.h"
+
+int main(void){
+
+}
