441104ff6a68d62154f1edecdb0ad2288fdf1b85
Coppermine-SP <son5121502@gmail.com>
2024-06-05 15:08:53 +0000

    feat: 연산자 구조체 정의

--- src/calc.c
+++ src/calc.c
@@ -1,4 +1,48 @@
 /*
     calc.c - bstCalc (2024-1 Data Structure Assignment)
     Copyright (C) 2024 Coppermine-SP - <국립창원대학교 컴퓨터공학과 20233063 손유찬>
-*/ 
+*/
+
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "calc.h"
+
+/*연산자: 연산자 구조체는 연산의 우선 순위와 문자, 연산 함수를 포함하고 있습니다.
+각 연산자는 모두 전역 상수로 정의되어 있으며, get_operator() function으로 각 연산 문자에 맞는 연산자 구조체를 const ptr로 제공받게 됩니다.
+연산 함수는 좌변, 우변과 결과를 저장할 intptr를 받고, 연산이 성공하면 true, 예외(division by zero)가 발생하면 false를 반환합니다.
+*/
+static bool add_func(int lhs, int rhs, int* out) { *out = lhs + rhs; return true; }
+static const operator_t add = {2, '+', &add_func};
+
+static bool minus_func(int lhs, int rhs, int* out) { *out = lhs - rhs; return true; }
+static const operator_t minus = {2, '-', &minus_func};
+
+static bool multiply_func(int lhs, int rhs, int* out) { *out = lhs * rhs; return true; }
+static const operator_t multiply = {3, '*', &multiply_func };
+
+static bool divide_func(int lhs, int rhs, int* out) {
+    if(rhs == 0) printf("Divide by zero\n"); return false;
+    *out = lhs / rhs; return true;
+}
+static const operator_t divide = {3, '/', &divide_func};
+
+static bool modulo_func(int lhs, int rhs, int* out) { 
+    if(rhs == 0) printf("Modulo by zero\n"); return false;
+    *out = lhs % rhs; return true;
+}
+static const operator_t modulo = {3, '%', &modulo_func};
+
+static bool power_func(int lhs, int rhs, int* out) {
+    int result = 1;
+    for(int i = 0; i < rhs; i++){ result *= lhs; }
+    *out = result; return true;
+}
+static const operator_t power = {1, '^', &power_func};
+static const operator_t* operators[] = {&add, &minus, &multiply, &divide, &modulo, &power, NULL};
+
+const operator_t* get_operator(char c){
+    const operator_t** element = operators;
+    while(*(element) != NULL) if((*element)->character == c) return *(element); else element++;
+    return NULL;
+}
--- src/calc.h
+++ src/calc.h
@@ -2,3 +2,17 @@
     calc.h - bstCalc (2024-1 Data Structure Assignment)
     Copyright (C) 2024 Coppermine-SP - <국립창원대학교 컴퓨터공학과 20233063 손유찬>
 */ 
+
+#pragma once
+#include <stdbool.h>
+
+typedef struct {
+    unsigned priority;
+    char character;
+    bool (*operation)(int, int, int*);
+} operator_t;
+
+/// @brief 연산자 문자에 맞는 연산자 참조를 반환합니다.
+/// @param c 연산자 문자
+/// @return (nullable) 연산자 참조.
+const operator_t* get_operator(char c);
