cecc330d47e04c520fccf3ff9b88e9c0d37a4fa8
Coppermine-SP <son5121502@gmail.com>
2024-06-08 18:44:07 +0000

    feat: 계산기 메인 로직 구현

--- src/main.c
+++ src/main.c
@@ -5,10 +5,22 @@
 
 #include <stdio.h>
 #include <stdbool.h>
-#include "bst.h"
 #include "calc.h"
 
 #define STRING_SIZE 100
+
+void print_array(node_t** arr){
+    int idx = 0;
+    node_t* element = arr[idx];
+
+    while(element != NULL){
+        if(element->op == NULL) printf("%d ", element->value);
+        else printf("%c ", element->op->character);
+
+        element = arr[++idx];
+    }
+    printf("\n");
+}
 
 int main(void){
     while(true){
@@ -18,6 +30,35 @@ int main(void){
         fgets(exp, STRING_SIZE, stdin);
     
         if(exp[0] == '\n') return 0;
-        printf("%s\n", exp);
-    }
+
+        //수식 파싱
+        node_t** inline_exp;
+        if(!parse_expression(exp, STRING_SIZE, &inline_exp)){
+            printf("Input Error\n\n");
+            continue;
+        }
+
+        print_array(inline_exp);
+
+        //infix => postfix
+        node_t** postfix_exp;
+        if(!infix_to_postfix(inline_exp, &postfix_exp)){
+            printf("Input Error\n\n");
+            continue;
+        }
+
+        print_array(postfix_exp);
+
+        //수식 트리 생성
+        node_t* root;
+        if(!make_expression_tree(postfix_exp, &root)){
+            printf("Input Error\n\n");
+            continue;
+        }
+
+        //수식 평가
+        bool exception_flag = false;
+        int result = evaluate(root, &exception_flag);
+        if(!exception_flag) printf("result: %d\n\n", result);
+  }
 }
