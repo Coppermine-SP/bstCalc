f61146431946e29d935d47910c9138941a21c7d0
Coppermine-SP <son5121502@gmail.com>
2024-06-11 10:22:19 +0000

    chore: 주석, 줄바꿈, 띄어쓰기 수정

--- src/calc.c
+++ src/calc.c
@@ -10,33 +10,33 @@
 #define MAX_TERMS 50
 
 /*
-연산자: 연산자 구조체는 연산의 우선 순위와 문자, 연산 함수를 포함하고 있습니다.
+    연산자: 연산자 구조체는 연산의 우선 순위와 문자, 연산 함수를 포함하고 있습니다.
 
-각 연산자는 모두 전역 상수로 정의되어 있으며, get_operator() function으로 각 연산 문자에 맞는 연산자 구조체를 const ptr로 제공받게 됩니다.
-연산 함수는 좌변, 우변과 결과를 저장할 intptr를 받고, 연산이 성공하면 true, 예외(division by zero)가 발생하면 false를 반환합니다.
+    각 연산자는 모두 전역 상수로 정의되어 있으며, get_operator() function으로 각 연산 문자에 맞는 연산자 구조체를 const ptr로 제공받게 됩니다.
+    연산 함수는 좌변, 우변과 결과를 저장할 intptr를 받고, 연산이 성공하면 true, 예외(division by zero)가 발생하면 false를 반환합니다.
 */
-static bool add_func(int lhs, int rhs, int* out) { *out = lhs + rhs; return true; }
+static bool add_func(int lhs, int rhs, int* out){ *out = lhs + rhs; return true; }
 static const operator_t add = {2, '+', &add_func};
 
-static bool minus_func(int lhs, int rhs, int* out) { *out = lhs - rhs; return true; }
+static bool minus_func(int lhs, int rhs, int* out){ *out = lhs - rhs; return true; }
 static const operator_t minus = {2, '-', &minus_func};
 
-static bool multiply_func(int lhs, int rhs, int* out) { *out = lhs * rhs; return true; }
+static bool multiply_func(int lhs, int rhs, int* out){ *out = lhs * rhs; return true; }
 static const operator_t multiply = {3, '*', &multiply_func };
 
-static bool divide_func(int lhs, int rhs, int* out) {
+static bool divide_func(int lhs, int rhs, int* out){
     if(rhs == 0) { printf("Divide by zero\n\n"); return false; }
     *out = lhs / rhs; return true;
 }
 static const operator_t divide = {3, '/', &divide_func};
 
-static bool modulo_func(int lhs, int rhs, int* out) { 
+static bool modulo_func(int lhs, int rhs, int* out){ 
     if(rhs == 0){ printf("Modulo by zero\n\n"); return false; }
     *out = lhs % rhs; return true;
 }
 static const operator_t modulo = {3, '%', &modulo_func};
 
-static bool power_func(int lhs, int rhs, int* out) {
+static bool power_func(int lhs, int rhs, int* out){
     int result = 1;
     for(int i = 0; i < rhs; i++){ result *= lhs; }
     *out = result; return true;
@@ -53,7 +53,6 @@ const operator_t* get_operator(char c){
     return NULL;
 }
 
-//파싱 및 정렬
 bool parse_expression(char* exp, int size, node_t*** out){
     char numeric_cache[MAX_DIGITS];
     int numeric_length = 0;
@@ -98,8 +97,7 @@ bool parse_expression(char* exp, int size, node_t*** out){
 
     //예외 처리시 모든 node dispose
     parse_exit:
-    if(terms > 0)
-        for(int i = 0; i <= terms; i++) dispose_node(arr[i]);
+    if(terms > 0) for(int i = 0; i <= terms; i++) dispose_node(arr[i]);
     free(arr);
     return false;
 }
@@ -157,8 +155,7 @@ bool make_expression_tree(node_t** exp, node_t** out){
         //예외 처리시 모든 node dispose
         idx = 0;
         element = exp[idx];
-        while(element != NULL)
-        {
+        while(element != NULL){
             dispose_node(element);
             element = exp[++idx];
         }
